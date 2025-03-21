48d0fcebdd41cfa6c485b133dd48ad4f7a359657
jijimama <ninisesekoma@gmail.com>
2025-03-21 09:26:20 +0900

    articleモデル追加

---
+++ app/models/article.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"encoding/json"
+
+	"go-api-newspaper/api"
+)
+
+type Article struct {
+	ID          int
+	Body        string
+	Year        int
+	Month       int
+	Day         int
+	NewspaperID int
+	Newspaper   *Newspaper
+}
