652ff32e41a7a516a3bb011696893ab3bcbae540
jijimama <ninisesekoma@gmail.com>
2025-03-31 05:54:31 +0900

    artickeハンドラーのファイル作成

---
+++ app/controllers/article.go
@@ -0,0 +1,13 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"go-api-newspaper/api"
+	"go-api-newspaper/app/models"
+	"go-api-newspaper/pkg/logger"
+)
+
+type ArticleHandler struct{}
