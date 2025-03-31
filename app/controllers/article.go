ba264c8221836d62f35f4baf56494f85d33ed1ef
jijimama <ninisesekoma@gmail.com>
2025-04-01 06:51:47 +0900

    CreateArticleメソッド作成

--- app/controllers/article.go
+++ app/controllers/article.go
@@ -11,3 +11,26 @@ import (
 )
 
 type ArticleHandler struct{}
+
+func (a *ArticleHandler) CreateArticle(c *gin.Context) {
+	var requestBody api.CreateArticleJSONRequestBody
+	if err := c.ShouldBindJSON(&requestBody); err != nil {
+		logger.Warn(err.Error())
+		c.JSON(http.StatusBadRequest, api.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	createdArticle, err := models.CreateArticle(
+		requestBody.Body,
+		requestBody.Year,
+		requestBody.Month,
+		requestBody.Day,
+		requestBody.NewspaperID)
+	if err != nil {
+		logger.Error(err.Error())
+		c.JSON(http.StatusInternalServerError, api.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, createdArticle)
+}
