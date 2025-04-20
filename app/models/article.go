edf85fc5dcb0d85947be168b6ce5d6f13ab213b4
jijimama <ninisesekoma@gmail.com>
2025-04-20 19:35:57 +0900

    Article MarshalJSONを作成

--- app/models/article.go
+++ app/models/article.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"encoding/json"
+	"go-api-newspaper/api"
+)
+
 type Article struct {
 	ID          int
 	Body        string
@@ -8,4 +13,14 @@ type Article struct {
 	Day         int
 	NewspaperID int
 	Newspaper   *Newspaper
+}
+
+func (a *Article) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&api.ArticleResponse{
+		Id:    a.ID,
+		Body:  a.Body,
+		Year:  a.Year,
+		Month: a.Month,
+		Day:   a.Day,
+	})
 }
