ae88bae575fd7749b7af9dca5ad86a3dae899040
jijimama <ninisesekoma@gmail.com>
2025-04-24 05:51:13 +0900

    CreateArticleを追加

--- app/models/article.go
+++ app/models/article.go
@@ -29,3 +29,23 @@ func (a *Article) MarshalJSON() ([]byte, error) {
 		},
 	})
 }
+
+func CreateArticle(body string, year int, month int, day int, newspaperID int) (*Article, error) {
+	newspaper, err := GetNewspaper(newspaperID)
+	if err != nil {
+		return nil, err
+	}
+
+	article := &Article{
+		Body:        body,
+		Year:        year,
+		Month:       month,
+		Day:         day,
+		NewspaperID: newspaperID,
+		Newspaper:   newspaper,
+	}
+	if err := DB.Create(article).Error; err != nil {
+		return nil, err
+	}
+	return article, nil
+}
