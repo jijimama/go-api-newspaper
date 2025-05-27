17f7fd5d316171f6e5873c90c3ca39b4b8877e1f
jijimama <ninisesekoma@gmail.com>
2025-05-28 06:01:33 +0900

    ShouldBindJSONで確認

--- app/controllers/article.go
+++ app/controllers/article.go
@@ -2,9 +2,16 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+
+	"go-api-newspaper/api"
 )
 
 type ArticleHandler struct{}
 
 func (a *ArticleHandler) CreateArticle(c *gin.Context) {
+	var requestBody api.CreateArticleJSONRequestBody
+	if err := c.ShouldBindJSON(&requestBody); err != nil {
+		c.JSON(400, api.ErrorResponse{Message: err.Error()})
+		return
+	}
 }
