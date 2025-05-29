e3eb65d73e4d925c6b3142f57c4a4b7bc38964af
jijimama <ninisesekoma@gmail.com>
2025-05-30 05:47:28 +0900

    loggerを使用する

--- app/controllers/article.go
+++ app/controllers/article.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 
 	"go-api-newspaper/api"
+	"go-api-newspaper/pkg/logger"
 )
 
 type ArticleHandler struct{}
@@ -11,7 +13,8 @@ type ArticleHandler struct{}
 func (a *ArticleHandler) CreateArticle(c *gin.Context) {
 	var requestBody api.CreateArticleJSONRequestBody
 	if err := c.ShouldBindJSON(&requestBody); err != nil {
-		c.JSON(400, api.ErrorResponse{Message: err.Error()})
+		logger.Warn(err.Error())
+		c.JSON(http.StatusBadRequest, api.ErrorResponse{Message: err.Error()})
 		return
 	}
 }
